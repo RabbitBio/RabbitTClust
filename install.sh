4500aadcb729c9a051a0b2713d84c75b5c08dcfe
xiaomingxu1995 <rryryry@126.com>
2022-02-24 08:12:31 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 set -x
 cd RabbitSketch &&
-mkdir build && cd build &&
+mkdir -p build && cd build &&
 cmake -DCXXAPI=ON -DCMAKE_INSTALL_PREFIX=. .. &&
 make && make install &&
 cd ../../ &&
 
 #make rabbitFX library
 cd RabbitFX && 
-mkdir build && cd build &&
+mkdir -p build && cd build &&
 cmake -DCMAKE_INSTALL_PREFIX=. .. &&
 make && make install && 
 cd ../../ &&
 
-mkdir build && cd build &&
+mkdir -p build && cd build &&
 cmake -DUSE_RABBITFX=ON .. && 
 make && make install &&
 cd ../ 
