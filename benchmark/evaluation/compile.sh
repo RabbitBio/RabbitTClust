ed19e6a3c02c19f91ec5695fbf51a780592c6009
xiaomingxu1995 <xiaoming.xu@mail.sdu.edu.cn>
2022-03-07 06:30:41 -0500

    remove obsolete F1-score evaluation indicators

--- benchmark/evaluation/compile.sh
+++ benchmark/evaluation/compile.sh
@@ -2,12 +2,11 @@
 if [ $# -ge 1 ] && [ $1 == "clean" ];
 then
 	set -x
-	rm mapGenome calGenome calResult2 calNMI calF1 calViral
+	rm mapGenome calGenome calNMI calF1 calViral
 else
 	set -x
 	g++ ./src/mapGenome.cpp -lz -o mapGenome
 	g++ ./src/calGenome.cpp -lz -o calGenome
-	g++ ./src/calResult2.cpp -lz -o calResult2
 	g++ ./src/calNMI.cpp -o calNMI
 	g++ ./src/calF1.cpp -o calF1
 	g++ ./src/calViral.cpp -o calViral
