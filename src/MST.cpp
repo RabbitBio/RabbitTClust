d63f6e50756fa6bdaea002138f370794047a8831
tripleldh <tong.z@mail.sdu.edu.cn>
2025-12-03 08:41:23 -0500

    fix newick

--- src/MST.cpp
+++ src/MST.cpp
@@ -30,6 +30,25 @@ inline double calr(double D, int k) {
 }
 
 
+struct DSU {
+    std::vector<int> p, r;
+    DSU(int n) : p(n), r(n, 0) {
+        std::iota(p.begin(), p.end(), 0);
+    }
+    int find(int x) {
+        return p[x] == x ? x : p[x] = find(p[x]);
+    }
+    int unite(int a, int b) {
+        a = find(a); b = find(b);
+        if (a == b) return a;
+        if (r[a] < r[b]) std::swap(a, b);
+        p[b] = a;
+        if (r[a] == r[b]) r[a]++;
+        return a;
+    }
+};
+
+
 vector<EdgeInfo> kruskalAlgorithm(vector<EdgeInfo>graph, int vertices){
     UnionFind uf(vertices);
     vector<EdgeInfo>spanningTree;
@@ -783,30 +802,30 @@ vector<EdgeInfo> modifyMST(vector<SketchInfo>& sketches, int start_index, int sk
 	return mst;
 }
 
-string build_kssd_newick_tree(vector<pair<int, double>>* G, bool* visited, const vector<KssdSketchInfo>& sketches, bool sketch_by_file, int node){
-	visited[node] = true;
-	//if(G[node].size() == 0)	return to_string(node);
-	string name;
-	if(sketch_by_file) name = sketches[node].fileName;
-	else name = sketches[node].seqInfo.name;
-	if(G[node].size() == 0){
-		return name;
-	}
-	string children("");
-	for(auto x : G[node]){
-		int cur_node = x.first;
-		double dist = x.second;
-		if(!visited[cur_node]){
-			string child = build_kssd_newick_tree(G, visited, sketches, sketch_by_file, cur_node);
-			children += child + ':' + to_string(dist) + ',';
-		}
-	}
-	//if(children.length() == 0) return to_string(node);
-	if(children.length() == 0) return name;
-	//children = '(' + children.substr(0, children.length()-1) + ')' + to_string(node);
-	children = '(' + children.substr(0, children.length()-1) + ')' + name;
-	return children;
-}
+// string build_kssd_newick_tree(vector<pair<int, double>>* G, bool* visited, const vector<KssdSketchInfo>& sketches, bool sketch_by_file, int node){
+// 	visited[node] = true;
+// 	//if(G[node].size() == 0)	return to_string(node);
+// 	string name;
+// 	if(sketch_by_file) name = sketches[node].fileName;
+// 	else name = sketches[node].seqInfo.name;
+// 	if(G[node].size() == 0){
+// 		return name;
+// 	}
+// 	string children("");
+// 	for(auto x : G[node]){
+// 		int cur_node = x.first;
+// 		double dist = x.second;
+// 		if(!visited[cur_node]){
+// 			string child = build_kssd_newick_tree(G, visited, sketches, sketch_by_file, cur_node);
+// 			children += child + ':' + to_string(dist) + ',';
+// 		}
+// 	}
+// 	//if(children.length() == 0) return to_string(node);
+// 	if(children.length() == 0) return name;
+// 	//children = '(' + children.substr(0, children.length()-1) + ')' + to_string(node);
+// 	children = '(' + children.substr(0, children.length()-1) + ')' + name;
+// 	return children;
+// }
 string build_newick_tree(vector<pair<int, double>>* G, bool* visited, const vector<SketchInfo>& sketches, bool sketch_by_file, int node){
 	visited[node] = true;
 	//if(G[node].size() == 0)	return to_string(node);
@@ -833,24 +852,24 @@ string build_newick_tree(vector<pair<int, double>>* G, bool* visited, const vect
 }
 
 
-string get_kssd_newick_tree(const vector<KssdSketchInfo>& sketches, const vector<EdgeInfo>& mst, bool sketch_by_file){
-	int vertice_number = sketches.size();
-	vector<pair<int, double>>* G = new vector<pair<int, double>> [vertice_number];
-	for(auto f : mst){
-		int u = f.preNode;
-		int v = f.sufNode;
-		double dist = f.dist;
-		G[u].push_back(make_pair(v, dist));
-		G[v].push_back(make_pair(u, dist));
-	}
-	bool* visited = new bool[vertice_number];
-	memset(visited, 0, vertice_number*sizeof(bool));
+// string get_kssd_newick_tree(const vector<KssdSketchInfo>& sketches, const vector<EdgeInfo>& mst, bool sketch_by_file){
+// 	int vertice_number = sketches.size();
+// 	vector<pair<int, double>>* G = new vector<pair<int, double>> [vertice_number];
+// 	for(auto f : mst){
+// 		int u = f.preNode;
+// 		int v = f.sufNode;
+// 		double dist = f.dist;
+// 		G[u].push_back(make_pair(v, dist));
+// 		G[v].push_back(make_pair(u, dist));
+// 	}
+// 	bool* visited = new bool[vertice_number];
+// 	memset(visited, 0, vertice_number*sizeof(bool));
 
-	string newick_tree = build_kssd_newick_tree(G, visited, sketches, sketch_by_file, 0);
-	newick_tree += ';';
+// 	string newick_tree = build_kssd_newick_tree(G, visited, sketches, sketch_by_file, 0);
+// 	newick_tree += ';';
 
-	return newick_tree;
-}
+// 	return newick_tree;
+// }
 
 string get_newick_tree(const vector<SketchInfo>& sketches, const vector<EdgeInfo>& mst, bool sketch_by_file){
 	int vertice_number = sketches.size();
@@ -871,5 +890,93 @@ string get_newick_tree(const vector<SketchInfo>& sketches, const vector<EdgeInfo
 	return newick_tree;
 }
 
+static std::string build_kssd_newick_tree(
+	int node,
+	const std::vector<std::vector<std::pair<int,double>>>& children,
+	const std::vector<KssdSketchInfo>& sketches,
+	bool sketch_by_file)
+{
+if (children[node].empty()) {
+	std::string name = sketch_by_file ? sketches[node].fileName
+									  : sketches[node].seqInfo.name;
+	return name;
+}
 
+std::string s = "(";
+for (size_t i = 0; i < children[node].size(); ++i) {
+	int child = children[node][i].first;
+	double bl  = children[node][i].second;
+	if (i > 0) s += ",";
+	s += build_kssd_newick_tree(child, children, sketches, sketch_by_file);
+	s += ":" + std::to_string(bl);
+}
+s += ")"; 
+return s;
+}
+
+std::string get_kssd_newick_tree(const std::vector<KssdSketchInfo>& sketches,
+							 const std::vector<EdgeInfo>& mst,
+							 bool sketch_by_file)
+{
+int N = sketches.size();
+if (N == 0) return ";";
+if (N == 1) {
+	std::string name = sketch_by_file ? sketches[0].fileName
+									  : sketches[0].seqInfo.name;
+	return name + ";";
+}
+
+std::vector<EdgeInfo> edges = mst;
+std::sort(edges.begin(), edges.end(),
+		  [](const EdgeInfo& a, const EdgeInfo& b){
+			  return a.dist < b.dist;
+		  });
+
+
+int maxNodes = 2 * N - 1;
+std::vector<std::vector<std::pair<int,double>>> children(maxNodes);
+std::vector<double> height(maxNodes, 0.0);     
+std::vector<int> repNode(maxNodes, -1);       
+for (int i = 0; i < N; ++i) {
+	repNode[i] = i;   
+}
+
+DSU dsu(N);
+int nextNode = N;      
+
+for (const auto& e : edges) {
+	int u = e.preNode;
+	int v = e.sufNode;
+	double w = e.dist;
+
+	int ru = dsu.find(u);
+	int rv = dsu.find(v);
+	if (ru == rv) continue; 
+
+	int nodeU = repNode[ru];
+	int nodeV = repNode[rv];
+
+	double hU = height[nodeU];
+	double hV = height[nodeV];
+	double h  = w;  
+
+	double blU = std::max(0.0, h - hU);
+	double blV = std::max(0.0, h - hV);
+
+	int newNode = nextNode++;
+	children[newNode].push_back({nodeU, blU});
+	children[newNode].push_back({nodeV, blV});
+	height[newNode] = h;
+
+	int rNew = dsu.unite(ru, rv);
+	repNode[rNew] = newNode;
+}
+
+int rootRep = dsu.find(0);
+int root    = repNode[rootRep];
+
+std::string newick_tree = build_kssd_newick_tree(root, children, sketches, sketch_by_file);
+newick_tree += ";";
+return newick_tree;
+}
 
